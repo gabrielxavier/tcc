74c26e91652f3304f68a92cbc3f2b42d93375748
Gabriel Xavier <gabriel.xavier@a2c.com.br>
2015-04-27 14:14:26 -0300

    Envio de e-mail recuperar a senha

--- views/admin/usuario/recuperarSenha.php
+++ views/admin/usuario/recuperarSenha.php
@@ -17,12 +17,24 @@ if( $_POST['matricula_recuperar'] )
         $nova_senha = $h->newPassword(6);
         $usuario->senha = md5( $nova_senha );
 
-         $crudUser->update($usuario)->executeQuery();
-         if( $crudUser->getExecutedQuery() )
-         {
-            $h->addFlashMessage('success','Senha enviada com sucesso! '.$nova_senha);
+        $mail = new PHPMailer;
+        $mail->setFrom('noexists@gmail.com', 'Portal@TCC');
+        $mail->Subject = 'Suporte @TCC';
+        $html = '<p>Olá <strong>'.$usuarioDB->nome.'</strong>, conforme sua solicitação estamos lhe enviando uma nova senha.';
+        $html .= '<p>Sua nova senha é <strong>'. $nova_senha.'</strong></p>';
+        $html .= '<p>Para sua segurança não responda este e-mail.</p>';
+        $html .= '<p>--</p>';
+        $html .= '<p>Portal @TCC</p>';
+        $mail->msgHTML($html);
+        $mail->addAddress($usuarioDB->email, $usuarioDB->nome);
+        $mail->send();
+
+        $crudUser->update($usuario)->executeQuery();
+        if( $crudUser->getExecutedQuery() )
+        {
+            $h->addFlashMessage('success','Senha enviada com sucesso!');
             $h->redirectFor('admin/login/index');
-         }else{
+        }else{
             $h->addFlashMessage('error','Não foi possível enviar a senha!');
             $h->redirectFor('admin/login/index');
         }
